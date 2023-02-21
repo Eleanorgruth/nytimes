c95806bfb05104cdb2411ba3ab277bbcd5022248
Eleanor Ruth <ellie.ruth@me.com>
2023-02-21 14:54:03 -0700

    Create Interfaces

---
+++ src/TypeUtilities/Interfaces.tsx
@@ -0,0 +1,33 @@
+export interface TopStoriesObject {
+  section: string
+  subsection: string
+  title: string
+  abstract: string
+  url: string
+  uri: string
+  byline: string
+  item_type: string
+  updated_date: string
+  created_date: string
+  published_date: string
+  material_type_facet: string
+  kicker: string
+  des_facet: string[]
+  org_facet: string[]
+  per_facet: string[]
+  geo_facet: string[]
+  multimedia: Multimedia[]
+}
+
+interface Multimedia {
+  url: string
+  format: string
+  height: number
+  width: number
+  type: string
+  subtype: string
+  caption: string
+  copyright: string
+  short_url: string
+
+}
