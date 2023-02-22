efb7bf9d5d1bb6b44ae75d1b5c1bfac8bb45cad3
Eleanor Ruth <ellie.ruth@me.com>
2023-02-22 09:05:20 -0700

    Style title

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,17 +1,14 @@
+import { Typography } from '@mui/material';
 import React, { FC } from 'react';
 import ArticleContainer from '../ArticleContainer/ArticleContainer';
 import './App.css';
 
-
 const App: FC = () => {
-//const [topStories, setTopStories] = useState<TopStoriesObject[]>([])
-  // useEffect(()=> {
-  //   fetchTopStories("home")
-  //     .then(data => setTopStories(data.results))
-  // }, [])
   return (
     <div className="App">
-      <h1>New York Times</h1>
+      <Typography variant='h2' component="h1">
+        New York Times
+      </Typography>
       <ArticleContainer/>
     </div>
   );
