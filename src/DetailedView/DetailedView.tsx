26ac538b46329e4dc4aff0f74900bca195b94a61
Eleanor Ruth <ellie.ruth@me.com>
2023-02-22 11:52:01 -0700

    create detailed view

---
+++ src/DetailedView/DetailedView.tsx
@@ -0,0 +1,39 @@
+import { Typography } from '@mui/material';
+import React, { FC } from 'react';
+import './DetailedView.css';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+const srcset = (image: string, size: number, rows = 1, cols = 1) => {
+  return {
+    src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
+    srcSet: `${image}?w=${size * cols}&h=${
+      size * rows
+    }&fit=crop&auto=format&dpr=2 2x`,
+  }
+}
+const DetailedView: FC = () => {
+  return (
+    <h1>Test</h1>
+  )
+  // return (
+  //   <ImageList
+  //     sx={{ width: 500, height: 450 }}
+  //     variant="quilted"
+  //     cols={4}
+  //     rowHeight={121}
+  //   >
+  //     {itemData.map((item) => (
+  //       <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+  //         <img
+  //           {...srcset(item.img, 121, item.rows, item.cols)}
+  //           alt={item.title}
+  //           loading="lazy"
+  //         />
+  //       </ImageListItem>
+  //     ))}
+  //   </ImageList>
+  // );
+}
+
+export default DetailedView;
