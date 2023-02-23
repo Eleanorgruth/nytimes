0107ecf5749f7ae4b547ee06d72d588b5023d13f
Eleanor Ruth <ellie.ruth@me.com>
2023-02-23 08:24:09 -0700

    Add NavDrawer

---
+++ src/Components/NavDrawer/NavDrawer.tsx
@@ -0,0 +1,65 @@
+import React, { FC } from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import { NavProps } from '../../TypeUtilities/Interfaces';
+
+const NavDrawer: FC<NavProps> = ({setTopStoriesCategory}) => {
+  const [open, setOpen] = React.useState(false);
+
+  const toggleDrawer =
+    (open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+      setOpen(open);
+    };
+
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {["arts", "automobiles", "books", "business", "fashion", "food", "health", "home", "insider", "magazine", "movies", "nyregion", "obituaries", "opinion", "politics", "realestate", "science", "sports", "sundayreview", "technology", "theater", "t-magazine", "travel", "upshot", "us", "world"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton onClick={()=> {
+              setTopStoriesCategory(text)
+            }}>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </Box>
+  );
+
+  return (
+    <div>
+      <MenuIcon onClick={toggleDrawer(true)}>Open Drawer</MenuIcon>
+      <Drawer
+        anchor={'left'}
+        open={open}
+        onClose={toggleDrawer(false)}
+      >
+        {list()}
+      </Drawer>
+    </div>
+  );
+}
+
+export default NavDrawer
