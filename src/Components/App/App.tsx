7666c0192bd09ed9bcc0ed1ac05bb78aa1809267
Eleanor Ruth <ellie.ruth@me.com>
2023-02-23 15:33:05 -0700

    Add Route for Error component

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,36 +2,41 @@ import ArticleContainer from '../ArticleContainer/ArticleContainer';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import DetailedView from '../DetailedView/DetailedView';
-import React, { FC , useEffect, useState} from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { fetchTopStories } from '../../apiCalls';
 import { TopStoriesObject } from '../../TypeUtilities/Interfaces';
 import Nav from '../Nav/Nav';
+import Error from '../Error/Error';
 
 const App: FC = () => {
   const [topStories, setTopStories] = useState<TopStoriesObject[]>([])
   const [topStoriesCategory, setTopStoriesCategory] = useState<string>("home")
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchTopStories(topStoriesCategory)
       .then(data => setTopStories(data.results))
   }, [topStoriesCategory])
 
   return (
     <div className="App">
-      <Nav setTopStoriesCategory={setTopStoriesCategory}/>
+      <Nav setTopStoriesCategory={setTopStoriesCategory} />
       <Routes>
         <Route
           path="/"
           element={
             <ArticleContainer
-            topStories={topStories}
+              topStories={topStories}
             />}
         />
         <Route
-          path="/:id"
+          path="/article/:id"
           element={<DetailedView
             topStories={topStories}
-            />}
+          />}
+        />
+        <Route
+          path="/*"
+          element={<Error/>}
         />
       </Routes>
     </div>
